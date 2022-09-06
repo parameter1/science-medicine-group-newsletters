4eb350fbda10138c48f9d785ee88095a4977c974
Jessica Wade <jessica.wade@parameter1.com>
2022-09-06 13:18:53 -0500

    Add tenants config folder

---
+++ tenants/all/config/core.js
@@ -0,0 +1,14 @@
+// const brands = require('./brands');
+// const emailX = require('./email-x');
+// const nativeX = require('./native-x');
+
+const config = {
+  // emailX,
+  // nativeX,
+  // dpm: {
+  //   emailx: {
+  //     enabled: true,
+  //   },
+};
+
+module.exports = config;
