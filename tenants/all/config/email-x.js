45d8ac281e277177593f0e38b1bbffe0123a7679
Jessica Wade <jessica.wade@parameter1.com>
2022-09-21 14:11:50 -0500

    Add config files with lab pulse set up

---
+++ tenants/all/config/email-x.js
@@ -0,0 +1,15 @@
+const EmailXConfiguration = require('@parameter1/base-cms-marko-newsletters-email-x/config');
+
+const config = new EmailXConfiguration(process.env.EMAILX_SERVE_URI || 'https://smg.serve.email-x.parameter1.com');
+
+config
+  .setAdUnits('lab-pulse-insight', [
+    {
+      name: 'ad-slot-1',
+      id: '632b08a058002aacbb708185',
+      width: 300,
+      height: 250,
+    },
+  ]);
+
+module.exports = config;
