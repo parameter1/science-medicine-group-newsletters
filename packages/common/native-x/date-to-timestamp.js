1e67b1a79cccdd47697221c7de1bccfb5d9a6ba0
Brian Miller <brian@parameter1.com>
2023-08-11 14:09:01 -0500

    NativeX Config & Fetch calls utilizing core package
    These along with the base-cms package upgrade/depchanges will make configuration and fetching elements shared within core pacakge.

--- packages/common/native-x/date-to-timestamp.js
+++
@@ -1,8 +0,0 @@
-module.exports = (date) => {
-  const dateInUTC = new Date(Date.UTC(
-    date.toDate().getFullYear(),
-    date.toDate().getMonth(),
-    date.toDate().getDate(),
-  ));
-  return dateInUTC.valueOf();
-};
